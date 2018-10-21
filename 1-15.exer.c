c57e82206fda2c1c65e86bf5af8d74a632a33797
szenadam <szenadam1@gmail.com>
2018-10-21 19:42:26 +0200

    Add 1-15 exercise

---
+++ 1-15.exer.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int toCelsius(int fahrenheit);
+void printTable(int lower, int upper, int step);
+
+int main() {
+    int lower, upper, step;
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printTable(lower, upper, step);
+
+    return 0;
+}
+
+int toCelsius(int fahrenheit) {
+    return 5 * (fahrenheit-32) / 9;
+}
+
+void printTable(int lower, int upper, int step) {
+    int fahr, celsius;
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = toCelsius(fahr);
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
