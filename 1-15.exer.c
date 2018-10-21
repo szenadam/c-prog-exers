47c04db3b85b62b6f66def5a0655d933369d8017
szenadam <szenadam1@gmail.com>
2018-10-21 20:35:39 +0200

    Add 1.8 main

--- 1-15.exer.c
+++ 1-15.exer.c
@@ -1,3 +1,7 @@
+/* Rewrite the temperature conversion program of 
+ * Section 1.2 to use a function for conversion.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
---
+++ 1.8.0.main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int power(int base, int n);
+
+int main() {
+
+    printf("%d", power(2, 10));
+
+    return 0;
+}
+
+/* power: raise base to n-th power; n >= 0; version 2 */
+int power(int base, int n) {
+    int p;
+    for (p = 1; n > 0 ; --n) {
+        p = p * base;
+    }
+    return p;
+}
