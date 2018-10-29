f6016591fe51cb6417f71f42c6b1d1e346da2bf5
szenadam <szenadam1@gmail.com>
2018-10-29 20:03:23 +0100

    Fix c++ comments in 1-18 as they are not allowed in ansi c

--- 1-18.exer.c
+++ 1-18.exer.c
@@ -6,20 +6,21 @@
 
 int getline(char s[], int max);
 
-// trailing = at the end of the line. A harder solution would be if
-// you want to remove beginning blanks as well.
-// empty line = one new line character
+/* trailing = at the end of the line. A harder solution would be if
+ * you want to remove beginning blanks as well.
+ * empty line = one new line character
+ */
 int main() {
     int i;
     int len;
     char line[MAXLINE];
 
     while ((len = getline(line, MAXLINE)) != 0 ) {
-        if (len > 1) { // its not an empty line
+        if (len > 1) { /* its not an empty line */
             for (i = len-1; (line[i] == ' ' || line[i] == '\t' || line[i] == '\n'); --i)
                 ;
-            line[++i] = '\n'; // Readd newline char and null terminator to line
-            line[++i] = '\0'; // null terminator is needed for printf
+            line[++i] = '\n'; /* Readd newline char and null terminator to line */
+            line[++i] = '\0'; /* null terminator is needed for printf */
             printf("%s", line);
         }
     }
