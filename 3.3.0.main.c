35a35bff0b6b4b0568ea647a89bc547c74b0f961
szenadam <szenadam1@gmail.com>
2018-11-12 11:47:12 +0100

    Add 3.3 main

---
+++ 3.3.0.main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int binsearch(int x, int v[], int n);
+
+int main() {
+    int a[5] = {1,2,3,4,5};
+    int result_position = binsearch(3, a, 5);
+    printf("%d\n", result_position);
+
+    return 0;
+}
+
+int binsearch(int x, int v[], int n) {
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+    while(low <= high){
+        mid = (low+high)/2;
+        if (x < v[mid])
+            high = mid+1;
+        else if (x > v[mid])
+            low = mid + 1;
+        else /*found match*/
+            return mid;
+    }
+    return -1; /*no match*/
+}
