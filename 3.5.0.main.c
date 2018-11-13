abcd75999afa5e78eed9ec79af81df603afb6f2c
szenadam <szenadam1@gmail.com>
2018-11-13 14:14:33 +0100

    Add shellsort to 3.5, use asertions

--- 3.5.0.main.c
+++ 3.5.0.main.c
@@ -3,9 +3,17 @@
 #include <assert.h>
 
 int atoi(char s[]);
+void shellsort(int v[], int n);
 
 int main() {
-    printf("%d", atoi("2345"));
+    assert(atoi("42") == 42);
+    assert(atoi("-42") == -42);
+
+    int a[5] = {5,4,3,2,1};
+    shellsort(a, 5);
+    for (int i = 0; i < 5; i++) {
+        assert(a[i] == i+1);
+    }
 
     return 0;
 }
@@ -21,4 +29,17 @@ int atoi(char s[]) {
     for (n = 0; isdigit(s[i]); i++)
         n = 10 * n + (s[i] - '0');
     return sign * n;
+}
+
+void shellsort(int v[], int n) {
+    int gap, i, j, temp;
+    for (gap = n/2; gap > 0; gap /= 2) {
+        for (i = gap; i < n; i++) {
+            for (j=i-gap; j>=0 && v[j]>v[j+gap]; j-=gap) {
+                temp = v[j];
+                v[j] = v[j+gap];
+                v[j+gap] = temp;
+            }
+        }
+    }
 }
