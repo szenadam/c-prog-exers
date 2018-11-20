5bb4ffc5539fcfe6e681dca724ef3a42b12d1565
szenadam <szenadam1@gmail.com>
2018-11-20 16:27:32 +0100

    Add 3.7 main, with trim func

---
+++ 3.7.main.c
@@ -0,0 +1,19 @@
+#include <string.h>
+
+int trim(char s[]);
+
+int main() {
+
+
+    return 0;
+}
+
+int trim(char s[]) {
+    int n;
+
+    for (n = strlen(s)-1; n >= 0; n--)
+        if (s[n] != ' ' && s[n] != '\t' && s[n] != '\n')
+            break;
+    s[n+1] = '\0';
+    return n;
+}
