fca7143c3c2eea2e3d2ca07445c1d81523139955
szenadam <szenadam1@gmail.com>
2018-11-21 15:57:18 +0100

    Add 4.2.0 main

---
+++ 4.2.0.main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <ctype.h>
+
+double atof(char s[]);
+
+int main() {
+    double d = atof("-123.456");
+    printf("%f", d);
+
+    return 0;
+}
+
+double atof(char s[]) {
+    double val, power;
+    int i, sign;
+
+    for (i = 0;isspace(s[i]); i++)
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        i++;
+    for (val = 0.0; isdigit(s[i]); i++)
+        val = 10.0 * val + (s[i] - '0');
+    if (s[i] == '.')
+        i++;
+    for (power = 1.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10;
+    }
+    return sign * val / power;
+}
